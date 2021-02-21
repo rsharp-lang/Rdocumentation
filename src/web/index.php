26fb2a9ab392f0836bd1dfa1dcdf98f865ce24b2
xieguigang <xie.guigang@gcmodeller.org>
2021-02-21 16:06:21 +0800

    add resource files

--- src/web/index.php
+++ src/web/index.php
@@ -4,6 +4,11 @@ include dirname(__DIR__) . "/../src/framework/bootstrap.php";
 
 class App {
 
+    /**
+     * R# documentation
+     * 
+     * Library of R# package documentation and techinical notes
+    */
     public function index() {
         View::Display();
     }
