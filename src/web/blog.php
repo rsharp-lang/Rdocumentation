e99c4d27710960ad0d3cb8a450163aab4c3704d0
xieguigang <xie.guigang@gcmodeller.org>
2021-02-21 19:31:39 +0800

    view blog post?

--- src/web/blog.php
+++ src/web/blog.php
@@ -3,8 +3,17 @@
 include dirname(__DIR__) . "/../src/framework/bootstrap.php";
 
 class App {
-
+    
+    /**
+     * View blog post
+     * 
+     * View blog post
+     * 
+     * @uses view
+    */
     public function index() {
-
+        View::Display([
+            "title" => "title"            
+        ]);
     }
 }
