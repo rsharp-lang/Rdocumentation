95d2accdbfe8092a65a0283094293f46c5a55bf5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-28 23:58:39 +0800

    create doucment files

--- .github/build-githubpage.R
+++ .github/build-githubpage.R
@@ -1,6 +1,14 @@
+imports "package_utils" from "devkit";
+
 require(REnv);
 
-const vignettes = `${dirname(@dir)}/vignettes`;
-const app_dir   = @HOME;
+const vignettes   = `${dirname(@dir)}/vignettes`;
+const app_dir     = @HOME;
+const clr_modules = [];
 
-
+for(dll_name in clr_modules) {
+    for(pkg_namespace in package_utils::parseDll(dll = `${app_dir}/${dll_name}`)) {
+        Rdocuments(pkg_namespace, outputdir = `${vignettes}/${basename(dll_name)}`, 
+            package = "R");
+    }
+}
