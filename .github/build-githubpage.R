d60249de8520640db4da03dd75e57dfded872ab7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-29 00:05:05 +0800

    add dll modules for run documents

--- .github/build-githubpage.R
+++ .github/build-githubpage.R
@@ -4,9 +4,14 @@ require(REnv);
 
 const vignettes   = `${dirname(@dir)}/vignettes`;
 const app_dir     = @HOME;
-const clr_modules = [];
+const clr_modules = ["REnv.dll" "Rlapack.dll" "roxygenNet.dll" "Rserve.dll" "Rserver.dll"
+                     "Rstudio.dll" "signalKit.dll" "snowFall.dll" "webKit.dll" "base.dll"
+                     "devkit.dll" "graphics.dll" "igraph.dll" "MLkit.dll" "njl.dll"
+                     "npy.dll" "nts.dll" "Parallel.dll" "RData.dll"];
 
 for(dll_name in clr_modules) {
+    str(dll_name);
+
     for(pkg_namespace in package_utils::parseDll(dll = `${app_dir}/${dll_name}`)) {
         Rdocuments(pkg_namespace, outputdir = `${vignettes}/${basename(dll_name)}`, 
             package = "R");
--- .github/build-githubpage.cmd
+++ .github/build-githubpage.cmd
@@ -3,4 +3,4 @@
 SET R_HOME=/GCModeller/src/R-sharp/App/net6.0-windows
 SET R_ENV="%R_HOME%/R#.exe"
 
-R_ENV ./build-githubpage.R
+%R_ENV% ./build-githubpage.R
