b948886dbfd28b1b9a7a9b0be496a6925ad3a455
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-29 00:21:01 +0800

    Update build-githubpage.R

--- .github/build-githubpage.R
+++ .github/build-githubpage.R
@@ -10,15 +10,28 @@ const clr_modules = ["REnv.dll" "Rlapack.dll" "roxygenNet.dll" "Rserve.dll" "Rse
                      "devkit.dll" "graphics.dll" "igraph.dll" "MLkit.dll" "njl.dll"
                      "npy.dll" "nts.dll" "Parallel.dll" "RData.dll"];
 
+const index = list();
+
 for(dll_name in clr_modules) {
+    let libs = [];
+
     str(dll_name);
 
     # generates the html help documents
     for(pkg_namespace in package_utils::parseDll(dll = `${app_dir}/${dll_name}`)) {
+        const libname = [pkg_namespace]::namespace;
+
         try({
             Rdocuments(pkg_namespace, 
-                outputdir = `${vignettes}/${basename(dll_name)}/${[pkg_namespace]::namespace}`, 
+                outputdir = `${vignettes}/${basename(dll_name)}/${libname}`, 
                 package = "R");
+            libs = append(libs, libname);
         });
     }
+    
+    index[[basename(dll_name)]] = libs;
+
+    NULL;
 }
+
+str(index);
