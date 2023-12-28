129ac228adfbc0f66bedf0889d9cbb7480959fd8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-29 00:08:39 +0800

    Update build-githubpage.R

--- .github/build-githubpage.R
+++ .github/build-githubpage.R
@@ -2,6 +2,7 @@ imports "package_utils" from "devkit";
 
 require(REnv);
 
+# set input/export
 const vignettes   = `${dirname(@dir)}/vignettes`;
 const app_dir     = @HOME;
 const clr_modules = ["REnv.dll" "Rlapack.dll" "roxygenNet.dll" "Rserve.dll" "Rserver.dll"
@@ -12,6 +13,7 @@ const clr_modules = ["REnv.dll" "Rlapack.dll" "roxygenNet.dll" "Rserve.dll" "Rse
 for(dll_name in clr_modules) {
     str(dll_name);
 
+    # generates the html help documents
     for(pkg_namespace in package_utils::parseDll(dll = `${app_dir}/${dll_name}`)) {
         Rdocuments(pkg_namespace, outputdir = `${vignettes}/${basename(dll_name)}`, 
             package = "R");
